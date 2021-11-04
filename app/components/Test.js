e8390c244b32129eab8690bfae5d63f87f22f80f
TKAYY729 <emmanueltweneboah71@gmail.com>
2021-11-04 11:09:36 -0400

    implemented a code snippet

--- app/components/Test.js
+++ app/components/Test.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
 function Test(props) {
- return (
+return (
    <View style={styles.container}></View>
-  );
+ );
 }
 const styles = StyleSheet.create({
 container: {}
 })
-
 export default  Test;
